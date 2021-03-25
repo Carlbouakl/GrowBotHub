2246dd3566b7f06fd0f41b294e8e903db764ba8b
Carl Bouakl <cjb11@mail.aub.edu>
2021-03-25 19:49:32 +0100

    Created empty classes

---
+++ AutomatedPlatformClass.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3.7
+import os
+import numpy as np
+
+class AUTOMATEDPLATFORM:
+
+    """INITIALIZATION"""
+    def __init__(self):
+        self.Pose # Location in x,y,z and yaw
+        self.Loaded = 0 # Carrying Growth module or not
+        self.Status = 'Standby' # Moving or Standby
+
+
+    """CLASS METHODS/FUNCTIONS"""
+    def move(self):
+        pass
+
+
+""" LISTENER """
+if __name__ == "__main__":
+    print('AUTOMATED_PLATFORM_CLASS')
